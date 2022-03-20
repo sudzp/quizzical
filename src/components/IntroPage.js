80bb6d13fb0cb2340b02ef6d71adde8e36ff40ba
sudhakar <prajapatisudhakar122@gmail.com>
2022-03-20 21:21:47 +0530

    Add Intro Page

---
+++ src/components/IntroPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+export default function IntroPage(){
+    return (
+        <div className='intro-page'>
+            <div className='intro-content'>
+          
+                <h1 className='title'>Quizzical</h1>
+                <div className='description'>Quiz App Built in React</div>
+                <div className='btn-container' >
+                    <button className='btn-start-quiz'>Start Quiz</button>
+                </div>
+                
+            </div>
+            
+            
+        </div>
+    )
+}
